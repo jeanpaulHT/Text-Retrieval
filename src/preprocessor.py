29d8f86eeefd323cdd6728e16ec274e4b6c3488d
jeanpaulHT <jean.huby@utec.edu.pe>
2021-07-04 04:06:17 -0500

    norm saved

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -37,8 +37,9 @@ class Preprocessor:
         for file in files:
             print(f"{n}:preprocess for {file} starting......")
             in_path, out_path = self.in_dir + file, self.out_dir + file.split('.')[0]
+            norm_path = f"{self.out_dir}../norm/" + str(file.split('.')[0])
             out_files.append(out_path)
-            self._preprocess_file(in_path, out_path)
+            self._preprocess_file(in_path, out_path,norm_path)
             print(f"preprocess for {file} done")
             n += 1
         print("preprocess completed")
@@ -60,8 +61,8 @@ class Preprocessor:
 
         return ' '.join(gen())
 
-    def _preprocess_file(self, in_path: str, out_path: str) -> None:
-        with open(in_path, encoding="utf-8") as f_in, open(out_path, "w+", encoding="utf-8") as f_out:
+    def _preprocess_file(self, in_path: str, out_path: str, norm_path : str) -> None:
+        with open(in_path, encoding="utf-8") as f_in, open(out_path, "w+", encoding="utf-8") as f_out, open(norm_path , "w+", encoding="utf-8") as n_out:
             json_data = json.load(f_in)
 
             for lineno, tweet in enumerate(json_data, 1):
@@ -69,8 +70,13 @@ class Preprocessor:
                 # extracting  and parsing tweet text and erasing skipped symbols0
                 res = str(tweet['id']) + ' '
                 line = Preprocessor._parse_line(tweet['content'], self.skipped_symbols)
+                line = self._preprocess_text(line)
+                num = len(re.findall(r'\w+', line))
 
-                res+= self._preprocess_text(line)
+                n_out.write(f"{res}{num}" + '\n')
+
+                res+= line
+
 
                 f_out.write(res + '\n')
 
