9bde1033c0fbb174595792642712e8fb6cda22d9
unknown <esteban.villacorta@utec,edu.pe>
2021-07-04 19:30:34 -0500

    done

--- src/main.py
+++ src/main.py
@@ -4,32 +4,34 @@ from queries import QueryEngine
 from os import listdir
 from os.path import isfile, join
 
-def main():
-    mypath = "../texts/raw"
-    json_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+mypath = "../texts/raw"
+json_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-    text_dir = "../texts/raw"
-    out_dir = "../texts/preprocessing"
-    stop_list = "../texts/stoplist.txt"
+text_dir = "../texts/raw"
+out_dir = "../texts/preprocessing"
+stop_list = "../texts/stoplist.txt"
     
-    index_tmp_dir = "../texts/index"
-    index_file = "../texts/index.txt"
+index_tmp_dir = "../texts/index"
+index_file = "../texts/index.txt"
 
+def get_out_and_norm_files(preprocessor: Preprocessor, json_files, rebuild):
+    if rebuild:
+        return preprocessor.preprocess(json_files)
+    else:
+        return preprocessor._locate(json_files)
+
+def get_query_engine(rebuild=True):
     preprocessor = Preprocessor(text_dir, out_dir, stop_list)
-    out_files, norm_files = preprocessor._locate(json_files)
+    
+    out_files, norm_files = get_out_and_norm_files(preprocessor, json_files, rebuild)
+    
+    index = Index(index_file,out_files, norm_files, index_tmp_dir, rebuild)
 
-    # out_files = preprocessor.preprocess(json_files)
+    return QueryEngine(preprocessor, index)
 
-    index = Index(
-        index_file=index_file,
-        files=out_files,
-        norm_files=norm_files,
-        tmp_dir=index_tmp_dir,
-        build=False              # change this to actually build
-    )
 
-    # #
-    engine = QueryEngine(preprocessor, index)
+def main():
+    engine = get_query_engine(rebuild=True)
     print(engine.search("muere martin vizcarra", 5))
 
 
